1cbb6237037726f0a01f50a98f93fb9e284b0003
yoonms0617 <yoonminwater@gmail.com>
2023-07-10 22:53:54 +0900

    feat: 사용자 아이디 및 비밀번호 불일치 예외코드 추가

--- backend/src/main/java/com/board/global/error/exception/ErrorType.java
+++ backend/src/main/java/com/board/global/error/exception/ErrorType.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorType {
 
     INVALID_INPUT_VALUE("ERR-COMMON001", HttpStatus.BAD_REQUEST.value()),
+    BAD_CREDENTIALS("ERR-AUTH001", HttpStatus.BAD_REQUEST.value()),
     DUPLICATE_NICKNAME("ERR-USER001", HttpStatus.CONFLICT.value()),
     DUPLICATE_USERNAME("ERR-USER002", HttpStatus.CONFLICT.value());
 
